533e0665dba4c5264326c543fe64b708054410f2
zomars <zomars@me.com>
2021-03-03 20:52:20 -0700

    Print the most-starred Python project on GitHub

--- python_repos.py
+++ python_repos.py
@@ -16,6 +16,11 @@ print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examine the first repository.
 repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+print("\nSelected information about first repository:")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Start: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
