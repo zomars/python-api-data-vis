07d106e98e83be5c0912ae7cadc265a6ddc05936
zomars <zomars@me.com>
2021-03-03 19:28:30 -0700

    Store API response

--- python_repos.py
+++ python_repos.py
@@ -5,3 +5,6 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
+
+# Store API response in a variable.
+response_dict = r.json()
